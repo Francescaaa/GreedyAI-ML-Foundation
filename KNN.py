2812a9cf0c7c7cd6e8575def374c4b87aabda07c
student126 <student126_0b994577ffb140c5@code.jdcloud.com>
2020-06-21 01:58:21 -0700

    Finalise KNN

--- KNN.py
+++ KNN.py
@@ -1,12 +1,13 @@
 from sklearn.model_selection import GridSearchCV  # 网格方式来搜索参数
 from sklearn.model_selection import KFold
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neighnors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from itertools import product
 from sklearn import datasets
 from collections import Counter  # 做投票
 from sklearn.model_selection import train_test_split
 import numpy as np
+import matplotlib.pyplot as plt
 
 iris = datasets.load_iris()
 X = iris.data
@@ -42,13 +43,13 @@ print("Accuracy is: %.3f" % (correct/len(X_test)))
 # 生成一些随机样本
 n_points = 100
 X1 = np.random.multivariate_normal([1, 50], [[1, 0], [0, 10]], n_points)
-X1 = np.random.multivariate_normal([2, 50], [[1, 0], [0, 10]], n_points)
+X2 = np.random.multivariate_normal([2, 50], [[1, 0], [0, 10]], n_points)
 X = np.concatenate([X1, X2])
 y = np.array([0]*n_points + [1]*n_points)
 print(X.shape, y.shape)
 
 # 训练9个K值不同的KNN
-clf = []
+clfs = []
 neighbors = [1, 3, 5, 9, 11, 13, 15, 17, 19]
 for i in range(len(neighbors)):
     clfs.append(KNeighborsClassifier(n_neighbors=neighbors[i]).fit(X, y))
@@ -56,11 +57,11 @@ for i in range(len(neighbors)):
 # 可视化结果
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arrange(x_min, x_max, 0.1),
-                     np.arrange(y_min, y_max, 0.1))
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                     np.arange(y_min, y_max, 0.1))
 
 f, axarr = plt.subplots(3, 3, sharex='col', sharey='row', figsize=(15, 12))
-for idx, clf, tt in zip(products([0, 1, 2], [0, 1, 2]),
+for idx, clf, tt in zip(product([0, 1, 2], [0, 1, 2]),
                         clfs,
                         ['KNN (k=%d)' % k for k in neighbors]):
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
