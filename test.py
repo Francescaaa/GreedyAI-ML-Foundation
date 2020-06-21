b26e4113fb4e7e910b66fc2f5b91b5922fa36e06
student126 <student126_0b994577ffb140c5@code.jdcloud.com>
2020-06-21 02:04:15 -0700

    Finalise linear regression

--- test.py
+++ test.py
@@ -5,9 +5,15 @@ data = np.array([[152, 51], [156, 53], [160, 54], [164, 55],
                  [184, 69], [188, 72]])
 print(data.shape)
 
+# Build a linear regression model
 x, y = data[:, 0].reshape(-1, 1), data[:, 1]
+# train the linear regression model
+regr = linear_model.LinearRegression()
+regr.fit(x, y)
 
-# 在二维空间画出身高和体重的分布图
+# Draw trained line
+plt.plot(x, regr.predict(x), color='blue')
+# Draw Scatter plot of height-weight
 plt.scatter(x, y, color='black')
 plt.xlabel('height (cm)')
 plt.ylabel('weight (kg)')
